7d7fe4657c276cad6a2da71ca777526fccbf2ec6
ShaymaaEssa <flowrathesky@gmail.com>
2025-02-12 03:15:08 +0200

    Products

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { ProductsService } from './../../core/services/products/products.service';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  private readonly productsService=inject(ProductsService);
+  
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.getProductsData();
+  }
+
+  getProductsData(){
+    this.productsService.getAllProducts().subscribe({
+      next:(res) =>{
+        console.log('here');
+        console.log(res.data);
+      },
+      error:(err)=>{
+        console.log(err.message)
+      }
+    });
+  }
 }
