4a18d7eccef260811051436647c6ae0b59e98e4e
ShaymaaEssa <flowrathesky@gmail.com>
2025-02-28 06:40:00 +0200

    category

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { FlowbiteService } from './core/services/flowbite/flowbite.service';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
 import { NgxSpinnerComponent } from 'ngx-spinner';
 
 @Component({
@@ -10,15 +11,26 @@ import { NgxSpinnerComponent } from 'ngx-spinner';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'FoodApp';
+  title = 'E-CommerceApp';
+  private initialized = false; // Prevent multiple initializations
+
 
   constructor(private FlowbiteService: FlowbiteService) {}
 
-  ngOnInit(): void {
-    this.FlowbiteService.loadFlowbite(flowbite => {
-      // Your custom code here
-      console.log('Flowbite loaded', flowbite);
-    });
-  }
+  // ngOnInit(): void {
+  //   this.FlowbiteService.loadFlowbite(flowbite => {
+  //     // Your custom code here
+  //     console.log('Flowbite loaded', flowbite);
+  //   });
+  // }
+
+  ngAfterViewChecked() {
+      if (!this.initialized) {
+        this.initialized = true;
+        setTimeout(() => {
+          initFlowbite();
+        }, 0);
+      }
+    }
 
 }
--- src/app/pages/categories/categories.component.ts
+++ src/app/pages/categories/categories.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { ICategory } from '../../shared/interfaces/icategory';
+import { CategoriesService } from './../../core/services/categories/categories.service';
+import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'app-categories',
@@ -6,6 +8,23 @@ import { Component } from '@angular/core';
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss'
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit{
+
+  private readonly categoriesService = inject(CategoriesService);
+  categoriesList: WritableSignal<ICategory[]> = signal ([]);
+  selectedCategory:WritableSignal<ICategory> = signal({}as ICategory);
+
+  ngOnInit(): void {
+    this.categoriesService.getAllCategories().subscribe({
+      next:(res)=>{
+        console.log(res.data);
+        this.categoriesList.set(res.data);
+      }
+    })
+  }
+
+  setSelectedCategory(category:ICategory){
+    this.selectedCategory.set(category);
+  }
 
 }
